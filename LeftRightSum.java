d46f05fa7eb944774bb16a95014763fabb645126
D G Sudheer <dgsudheer@gmail.com>
2024-04-04 23:32:39 +0530

    prb2574

--- LeftRightSum.java
+++ LeftRightSum.java
@@ -40,4 +40,14 @@ public class LeftRightSum {
         }
         return nums;
     }
+
+    public int[] leftRigthDifference(int[] nums) {
+        int lsum[] = new int[nums.length];
+        int rsum[] = new int[nums.length];
+        int res[] = new int[nums.length];
+        for(int i=1; i<nums.length; i++)    lsum[i] = lsum[i-1]+nums[i-1];
+        for(int i=nums.length-2; i>=0; i--)    rsum[i] = rsum[i+1] + nums[i+1];
+        for(int i=0; i<nums.length; i++)   res[i] = Math.abs(lsum[i]-rsum[i]);
+        return res;
+    }
 }
